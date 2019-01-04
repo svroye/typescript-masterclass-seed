60e551643a290c7f6c413c54dc355052818406d3
svroye <vanroyesteven@gmail.com>
2019-01-04 14:43:50 +0100

    this keyword

---
+++ src/1-this.ts
@@ -0,0 +1,33 @@
+/**
+ * INTRO TO 'this'
+ */
+
+ /** 
+function myFunction() {
+    console.log('Function:::', this);
+}
+
+// this --> window 
+myFunction();
+
+const obj = {
+    myMethod(){
+        console.log('Object:::', this);
+    }
+}
+
+// this --> obj
+obj.myMethod();
+
+class MyClass {
+    
+    myMethod() {
+        console.log('Class:::', this);
+    }
+}
+
+const myObj = new MyClass();
+// this --> MyClass
+myObj.myMethod();
+
+*/
---
+++ src/2-this-call-apply-bind.ts
@@ -0,0 +1,42 @@
+/**
+ * THis and the functionc CALL, APPLY and BIND
+ */
+
+ /** 
+function myFunction() {
+    console.log('Function:::', this);
+}
+
+const obj = {
+    myMethod(){
+        console.log('Object:::', this);
+    }
+}
+
+
+
+// we can change the this keyword when we call the function by using
+// call on the function with the argument being the context for 'this'
+myFunction();
+myFunction.call(obj);
+myFunction.call([]);
+
+// when calling a function with arguments, pass them AFTER the context
+function newFunction(text: string, arg2: string){
+    console.log(this, text, arg2);
+}
+
+newFunction.call(obj, "Hello", "There");
+
+// apply is similar, but requires an array for the arguments
+newFunction.apply(obj, ["Hello", "There"]);
+
+// with bind, you can set the context ahead of time. It doesn't invoke the function
+// but returns a new function with the context change set up
+const bindFunction = newFunction.bind(obj);
+
+// the arguments correspond with the original newFunction
+// definition on which the bind was performed. It can also be given when calling
+// bind, but this is not recommended
+bindFunction("Hello", "123");
+*/
